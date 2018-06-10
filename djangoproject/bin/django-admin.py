5268ba970d0910c95b2424362332fba17e799f27
Andres <andresbe.be@gmail.com>
2018-06-10 17:56:56 -0300

    first commit

---
+++ djangoproject/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/linux/Nextcloud/Programacion_II/proyectodjango/djangoproject/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ proyecto/web_page_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WebPageAppConfig(AppConfig):
+    name = 'web_page_app'
